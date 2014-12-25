4ee4e312f9356312e2c84d17110805c9e6c2b039
Goatus123 <oskar@spddo.co.uk>
2014-12-25 11:20:38 +0000

    Date

--- zoearoundtheworld/www/static/modules/entryform/main.js
+++ zoearoundtheworld/www/static/modules/entryform/main.js
@@ -2,17 +2,37 @@
  * Created by OskarBunyan on 08/12/14.
  */
 define([
-    "knockout"
+    "knockout", "moment"
     ],
-    function(ko) {
+    function(ko, moment) {
         function Panel(params) {
             this.appl = params.appl;
             this.country = params.country;
             this.title = ko.observable();
-            this.date = ko.observable();
+            this.day = ko.observable();
+            this.month = ko.observable();
+            this.year = ko.observable();
             this.text = ko.observable();
             this.success = ko.observable();
             this.error = ko.observable();
+            this.dayopt = ko.pureComputed(function(){
+                if(this.month() && this.year()){
+                    var days = [];
+                    var i, items = moment().month(this.month()).year(this.year()).daysInMonth();
+                    for(i=1; i<=items; i++){
+                        days.push(i);
+                    }
+                    return days;
+                }
+            }, this);
+            this.monthopt = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+            var years = [];
+            var i, end = 2015;
+            for(i=1990;i<=end;i++){
+                years.push(i);
+            }
+            this.yearopt = years;
         }
 
         Panel.prototype.save = function(){
