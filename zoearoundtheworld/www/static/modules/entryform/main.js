1ff87b59aef0033337a19b3673d1c938e9d88c35
Goatus123 <oskar@spddo.co.uk>
2014-12-25 13:30:07 +0000

    Post date

--- zoearoundtheworld/www/static/modules/entryform/main.js
+++ zoearoundtheworld/www/static/modules/entryform/main.js
@@ -6,6 +6,8 @@ define([
     ],
     function(ko, moment) {
         function Panel(params) {
+            window.new_entry_panel = this;
+
             this.appl = params.appl;
             this.country = params.country;
             this.title = ko.observable();
@@ -25,21 +27,43 @@ define([
                     return days;
                 }
             }, this);
-            this.monthopt = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+            this.monthopt = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
             var years = [];
-            var i, end = 2015;
-            for(i=1990;i<=end;i++){
+            var i;
+            for(i=2015;i>=1990;i--){
                 years.push(i);
             }
             this.yearopt = years;
+            this.entrydate = ko.computed({
+                read:function(){
+                    debugger;
+                    var m = moment({
+                        year:this.year(),
+                        month:this.monthopt.indexOf(this.month()),
+                        day:this.day()
+                    });
+                    console.log("read",m.format());
+                    return m;
+                },
+                write: function(value){
+                    var m = moment(value);
+                    this.year(m.year());
+                    this.month(this.monthopt[m.month()]);
+                    this.day(m.date());
+                    console.log("write",this.entrydate().format());
+                },
+                owner: this,
+                deferEvaluation: true
+            });
+            this.entrydate(moment());
         }
 
         Panel.prototype.save = function(){
             this.appl.send("new_entry",
                 {
                     "title": this.title(),
-                    "date": this.date(),
+                    "date": this.entrydate().format("YYYY-MM-DD"),
                     "country": this.country(),
                     "text": this.text()
                 },
